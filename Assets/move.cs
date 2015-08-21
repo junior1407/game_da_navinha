722389acbd5ceb6b92e5f32b4f98cffa7cef9450
Valdir de Souza Junior <jr140797@gmail.com>
2015-08-21 10:40:48 -0300

    srry niichan

---
+++ Assets/move.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class move : MonoBehaviour {
+	public float speed = 10.0f;
+	public float tilt=1.1f;
+				
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
+		GetComponent<Rigidbody>().velocity = movement * speed;
+		
+		GetComponent<Rigidbody>().position = new Vector3 
+			(
+				Mathf.Clamp (GetComponent<Rigidbody>().position.x,-1.5f, 1.5f), 
+				0.0f, 
+				0.0f
+				);
+		
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler (0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+	}
+		}
+	
