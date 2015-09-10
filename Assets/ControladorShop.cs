53f4b11170c590ec582be3d2a8b056cbd907320f
Valdir de Souza Junior <jr140797@gmail.com>
2015-09-10 20:47:20 -0300

    SHOP ORGANIZADO

---
+++ Assets/ControladorShop.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControladorShop : MonoBehaviour {
+
+
+
+	public void apertarBotao(){
+		Debug.Log ("Apertado");
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
